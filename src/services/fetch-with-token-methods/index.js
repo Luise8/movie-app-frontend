ea33d9d95cd5338c1ee515c76279441089cfb11a
luis <luistenan@gmail.com>
2023-06-08 19:24:30 -0400

    Add services to fetch data with reacptcha token and add tests
    Add fetch functions to get, put, delete and post data with the token of google recaptcha.
    Add tests.

---
+++ src/services/fetch-with-token-methods/index.js
@@ -0,0 +1,139 @@
+import { recaptchaKey } from '../settings';
+
+// FETCH METHODS WITH RECAPTCHA TOKEN //
+
+// GET
+export async function getWithToken(url) {
+  const token = await new Promise((resolve, reject) => {
+    window.grecaptcha.ready(async () => {
+      try {
+        const newToken = await window.grecaptcha.execute(recaptchaKey, { action: 'submit' });
+
+        resolve(newToken);
+      } catch (error) {
+        reject(new Error(error));
+      }
+    });
+  });
+
+  const response = await fetch(url, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      recaptcha: token,
+    },
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+  const error = new Error(response);
+  error.status = response.status;
+  error.message = response.statusText;
+  throw error;
+}
+
+// POST
+export async function postWithToken({ url, body }) {
+  const token = await new Promise((resolve, reject) => {
+    window.grecaptcha.ready(async () => {
+      try {
+        const newToken = await window.grecaptcha.execute(recaptchaKey, { action: 'submit' });
+
+        resolve(newToken);
+      } catch (error) {
+        reject(new Error(error));
+      }
+    });
+  });
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      recaptcha: token,
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+  const error = new Error(response);
+  error.status = response.status;
+  error.message = response.statusText;
+  throw error;
+}
+
+// DELETE
+export async function deleteWithToken(url) {
+  const token = await new Promise((resolve, reject) => {
+    window.grecaptcha.ready(async () => {
+      try {
+        const newToken = await window.grecaptcha.execute(recaptchaKey, { action: 'submit' });
+
+        resolve(newToken);
+      } catch (error) {
+        reject(new Error(error));
+      }
+    });
+  });
+
+  const response = await fetch(url, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      recaptcha: token,
+    },
+  });
+
+  if (response.ok) {
+    return response;
+  }
+  const error = new Error(response);
+  error.status = response.status;
+  error.message = response.statusText;
+  throw error;
+}
+
+// PUT
+export async function putWithToken({ url, body }) {
+  const token = await new Promise((resolve, reject) => {
+    window.grecaptcha.ready(async () => {
+      try {
+        const newToken = await window.grecaptcha.execute(recaptchaKey, { action: 'submit' });
+
+        resolve(newToken);
+      } catch (error) {
+        reject(new Error(error));
+      }
+    });
+  });
+
+  const response = await fetch(url, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      recaptcha: token,
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+  const error = new Error(response);
+  error.status = response.status;
+  error.message = response.statusText;
+  throw error;
+}
---
+++ src/services/fetch-with-token-methods/index.test.js
@@ -0,0 +1,188 @@
+import {
+  getWithToken, postWithToken, deleteWithToken, putWithToken,
+} from './index';
+
+jest.mock('../settings', () => ({
+  recaptchaKey: '123',
+}));
+
+// mock fetch
+global.fetch = jest.fn();
+
+const recaptchaToken = '123';
+// mock google recaptcha functions
+global.grecaptcha = {
+  ready: jest.fn((callback) => callback()),
+  execute: jest.fn(() => Promise.resolve(recaptchaToken)),
+};
+
+const url = 'url';
+const initialtOtions = {
+  credentials: 'include',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    recaptcha: recaptchaToken,
+  },
+};
+function generateOptions({ method, body }) {
+  return { ...initialtOtions, method, body };
+}
+beforeEach(() => {
+  fetch.mockClear();
+  global.grecaptcha.ready.mockClear();
+  global.grecaptcha.execute.mockClear();
+});
+
+describe('get', () => {
+  const body = {
+    data: 'some data',
+  };
+  beforeAll(() => {
+    fetch.mockImplementation(() => Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(body),
+    }));
+  });
+
+  it('called with valid arguments', async () => {
+    const data = await getWithToken(url);
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(url, generateOptions({ method: 'GET' }));
+    expect(data).toBe(body);
+  });
+
+  it('rejecting with the error when the exectue method get some error and don\'t call fetch function', async () => {
+    global.grecaptcha.execute.mockImplementationOnce(() => Promise.reject(new Error('error')));
+    await expect(getWithToken(url)).rejects.toThrow('error');
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).not.toBeCalled();
+  });
+
+  it('rejecting with the error when fetch throw some expection', async () => {
+    fetch.mockImplementationOnce(() => Promise.reject(new Error('error')));
+    await expect(getWithToken(url)).rejects.toThrow('error');
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('post', () => {
+  const body = {
+    data: 'some data',
+  };
+  const method = 'POST';
+  beforeAll(() => {
+    fetch.mockImplementation(() => Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(body),
+    }));
+  });
+
+  it('called with valid arguments', async () => {
+    const data = await postWithToken({ url, body });
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch)
+      .toHaveBeenCalledWith(url, generateOptions({ method, body: JSON.stringify(body) }));
+    expect(data).toBe(body);
+  });
+
+  it('fails rejecting with the error when the exectue method get some error and don\'t call fetch function', async () => {
+    global.grecaptcha.execute.mockImplementationOnce(() => Promise.reject(new Error('error')));
+    await expect(postWithToken({ url, body })).rejects.toThrow('error');
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).not.toBeCalled();
+  });
+
+  it('rejecting with the error when fetch throw some expection', async () => {
+    fetch.mockImplementationOnce(() => Promise.reject(new Error('error')));
+    await expect(postWithToken({ url, body })).rejects.toThrow('error');
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('put', () => {
+  const body = {
+    data: 'some data',
+  };
+  const method = 'PUT';
+  beforeAll(() => {
+    fetch.mockImplementation(() => Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(body),
+    }));
+  });
+
+  it('called with valid arguments', async () => {
+    const data = await putWithToken({ url, body });
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch)
+      .toHaveBeenCalledWith(url, generateOptions({ method, body: JSON.stringify(body) }));
+    expect(data).toBe(body);
+  });
+
+  it('fails rejecting with the error when the exectue method get some error and don\'t call fetch function', async () => {
+    global.grecaptcha.execute.mockImplementationOnce(() => Promise.reject(new Error('error')));
+    await expect(putWithToken({ url, body })).rejects.toThrow('error');
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).not.toBeCalled();
+  });
+
+  it('rejecting with the error when fetch throw some expection', async () => {
+    fetch.mockImplementationOnce(() => Promise.reject(new Error('error')));
+    await expect(putWithToken({ url, body })).rejects.toThrow('error');
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('delete', () => {
+  const method = 'DELETE';
+  beforeAll(() => {
+    fetch.mockImplementation(() => Promise.resolve({
+      ok: true,
+      status: 204,
+    }));
+  });
+
+  it('called with valid arguments', async () => {
+    const response = await deleteWithToken(url);
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(url, generateOptions({ method }));
+    expect(response.status).toBe(204);
+  });
+
+  it('rejecting with the error when the exectue method get some error and don\'t call fetch function', async () => {
+    global.grecaptcha.execute.mockImplementationOnce(() => Promise.reject(new Error('error')));
+    await expect(deleteWithToken(url)).rejects.toThrow('error');
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).not.toBeCalled();
+  });
+
+  it('rejecting with the error when fetch throw some expection', async () => {
+    fetch.mockImplementationOnce(() => Promise.reject(new Error('error')));
+    await expect(deleteWithToken(url)).rejects.toThrow('error');
+    expect(global.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+});
