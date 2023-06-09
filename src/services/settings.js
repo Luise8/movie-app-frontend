d1b5d5171807f14c8a65423a070fb7f4a39040b9
luis <luistenan@gmail.com>
2023-06-09 11:39:32 -0400

    Fix wrong urls in settings

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,60 +1,62 @@
-const apiURL = import.meta.env.VITE_API_URL;
+const baseApiURL = import.meta.env.VITE_API_URL;
 const recaptchaKey = import.meta.env.VITE_RECAPTCHA_KEY;
 const apiURLs = {
 
   // User
-  createUser: `${apiURL}/users`,
-  deleteUser: (id) => `${apiURL}/users/${id}`,
-  editUser: (id) => `${apiURL}/users/${id}`,
+  createUser: `${baseApiURL}/users`,
+  deleteUser: (id) => `${baseApiURL}/users/${id}`,
+  editUser: (id) => `${baseApiURL}/users/${id}`,
 
   // Get resorces of user
-  getUser: (id) => `${apiURL}/users/${id}`,
-  getRatesUser: ({ id, page }) => `${apiURL}/users/${id}/rates?page=${page || 1}`,
-  getReviewsUser: ({ id, page }) => `${apiURL}/users/${id}/reviews?page=${page || 1}`,
-  getWatchlistUser: (id) => `${apiURL}/users/${id}/watchlist`,
-  getWatchlistUserLight: (id) => `${apiURL}/users/${id}/watchlist?light=true`,
-  getListsUser: ({ id, page }) => `${apiURL}/users/${id}/lists?page=${page || 1}`,
-  getOneListUser: ({ userId, listId }) => `${apiURL}/users/${userId}/lists/${listId}`,
-  getOneListUserLight: ({ userId, listId }) => `${apiURL}/users/${userId}/lists/${listId}?light=true`,
+  getUser: (id) => `${baseApiURL}/users/${id}`,
+  getRatesUser: ({ id, page }) => `${baseApiURL}/users/${id}/rates?page=${page}&pageSize=20`,
+  getReviewsUser: ({ id, page }) => `${baseApiURL}/users/${id}/reviews?page=${page}&pageSize=20`,
+  getWatchlistUser: ({ id, page }) => `${baseApiURL}/users/${id}/watchlist?page=${page}&pageSize=20`,
+  getWatchlistUserLight: (id) => `${baseApiURL}/users/${id}/watchlist?light=true`,
+  getListsUser: ({ id, page }) => `${baseApiURL}/users/${id}/lists?page=${page}&pageSize=20`,
+  getOneListUser: ({
+    userId, listId, page,
+  }) => `${baseApiURL}/users/${userId}/lists/${listId}?page=${page}&pageSize=20`,
+  getOneListUserLight: ({ userId, listId }) => `${baseApiURL}/users/${userId}/lists/${listId}?light=true`,
 
   // List
-  createList: (id) => `${apiURL}/users/${id}/lists`,
-  editList: ({ userId, listId }) => `${apiURL}/users/${userId}/lists/${listId}`,
-  deleteList: ({ userId, listId }) => `${apiURL}/users/${userId}/lists/${listId}`,
+  createList: (id) => `${baseApiURL}/users/${id}/lists`,
+  editList: ({ userId, listId }) => `${baseApiURL}/users/${userId}/lists/${listId}`,
+  deleteList: ({ userId, listId }) => `${baseApiURL}/users/${userId}/lists/${listId}`,
 
   // Watchlist
-  editWatchlist: (id) => `${apiURL}/users/${id}/watchlist`,
+  editWatchlist: (id) => `${baseApiURL}/users/${id}/watchlist`,
 
   // Get movie data
-  getMovieDetail: (id) => `${apiURL}/movies/${id}/detail`,
-  getMovieTmdb: (id) => `${apiURL}/movies/${id}/tmdb`,
-  getReviewsMovie: ({ id, page }) => `${apiURL}/movies/${id}/reviews?page=${page || 1}`,
-  getOneReviewMovie: ({ movieId, reviewId }) => `${apiURL}/movies/${movieId}/reviews/${reviewId}`,
-  getLatestMovies: (page) => `${apiURL}/movies/latest?page=${page || 1}`,
-  getPopularMovies: (page) => `${apiURL}/movies/popular?page=${page || 1}`,
-  getTrendingMovies: (page) => `${apiURL}/movies/trending?page=${page || 1}`,
-  getRatedMovies: (page) => `${apiURL}/movies/rated?page=${page || 1}`,
-  getMoviesByGenre: ({ page, genres }) => `${apiURL}/movies/genre?genres=${genres}&page=${page || 1}`,
+  getMovieDetail: (id) => `${baseApiURL}/movies/${id}/detail`,
+  getMovieTmdb: (id) => `${baseApiURL}/movies/${id}/tmdb`,
+  getReviewsMovie: ({ id, page }) => `${baseApiURL}/movies/${id}/reviews?page=${page}&pageSize=20`,
+  getOneReviewMovie: ({ movieId, reviewId }) => `${baseApiURL}/movies/${movieId}/reviews/${reviewId}`,
+  getLatestMovies: (page) => `${baseApiURL}/movies/latest?page=${page}`,
+  getPopularMovies: (page) => `${baseApiURL}/movies/popular?page=${page}`,
+  getTrendingMovies: (page) => `${baseApiURL}/movies/trending?page=${page}`,
+  getRatedMovies: (page) => `${baseApiURL}/movies/rated?page=${page}`,
+  getMoviesByGenre: ({ page, genres }) => `${baseApiURL}/movies/genre?genres=${genres}&page=${page}`,
   // Enconded query
-  getMoviesBySearch: ({ page, query }) => `${apiURL}/movies/search?query=${encodeURIComponent(query)}&page=${page || 1}`,
-  getSpecificReviewUser: (id) => `${apiURL}/movies/${id}/reviewUser`,
-  getSpecificRateUser: (id) => `${apiURL}/movies/${id}/ratewUser`,
+  getMoviesBySearch: ({ page, query }) => `${baseApiURL}/movies/search?query=${encodeURIComponent(query)}&page=${page}`,
+  getSpecificReviewUser: (id) => `${baseApiURL}/movies/${id}/reviewUser`,
+  getSpecificRateUser: (id) => `${baseApiURL}/movies/${id}/ratewUser`,
 
   // Reviews
-  createReview: (id) => `${apiURL}/movies/${id}/reviews`,
-  editReview: ({ movieId, reviewId }) => `${apiURL}/movies/${movieId}/reviews/${reviewId}`,
-  deleteReview: ({ movieId, reviewId }) => `${apiURL}/movies/${movieId}/reviews/${reviewId}`,
+  createReview: (id) => `${baseApiURL}/movies/${id}/reviews`,
+  editReview: ({ movieId, reviewId }) => `${baseApiURL}/movies/${movieId}/reviews/${reviewId}`,
+  deleteReview: ({ movieId, reviewId }) => `${baseApiURL}/movies/${movieId}/reviews/${reviewId}`,
 
   // Rate
-  createRate: (id) => `${apiURL}/movies/${id}/rates`,
-  editRate: ({ movieId, rateId }) => `${apiURL}/movies/${movieId}/rates/${rateId}`,
-  deleteRate: ({ movieId, rateId }) => `${apiURL}/movies/${movieId}/rates/${rateId}`,
+  createRate: (id) => `${baseApiURL}/movies/${id}/rates`,
+  editRate: ({ movieId, rateId }) => `${baseApiURL}/movies/${movieId}/rates/${rateId}`,
+  deleteRate: ({ movieId, rateId }) => `${baseApiURL}/movies/${movieId}/rates/${rateId}`,
 
   // Auth
-  authLogIn: `${apiURL}/auth/login`,
-  authLogoOut: `${apiURL}/auth/logout`,
-  authStatus: `${apiURL}/auth/status`,
+  authLogIn: `${baseApiURL}/auth/login`,
+  authLogoOut: `${baseApiURL}/auth/logout`,
+  authStatus: `${baseApiURL}/auth/status`,
 
 };
 
-export { apiURLs, recaptchaKey };
+export { apiURLs, recaptchaKey, baseApiURL };
