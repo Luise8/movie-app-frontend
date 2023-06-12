5ead882dfc767a8fc8812c96ad72823adce10007
luis <luistenan@gmail.com>
2023-06-12 14:09:37 -0400

    Add empty necessary pages

---
+++ src/pages/rated/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Rated() {
+  return (
+    <div>Rated</div>
+  );
+}
---
+++ src/pages/watchlist-form/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function WatchlistForm() {
+  return (
+    <div>WatchlistForm</div>
+  );
+}
