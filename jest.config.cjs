038570e0d363390f899be75c325bc4d2b6f0a7ea
luis <luistenan@gmail.com>
2023-06-14 16:55:58 -0400

    Add jest configuration to be able to test files with css imports and other formats
    Add configuration to be able to test files with imports of files with css, less, images, audio, video, extensions.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test-config/mocks/file-mock.js',
+    '\\.(css|less)$': '<rootDir>/test-config/mocks/styles-mock.js',
     '^src/(.*)$': '<rootDir>/src/$1',
   },
 
