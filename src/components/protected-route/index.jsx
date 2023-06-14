c6a9fff4e83470bf0e90fe1eb102794e704a1809
luis <luistenan@gmail.com>
2023-06-14 12:06:27 -0400

    Add ProtectedRoute component to protect auth routes

---
+++ src/components/protected-route/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useUserAuth } from 'src/context/auth';
+import Loading from 'src/components/loading';
+
+function ProtectedRoute({ children }) {
+  const { user } = useUserAuth();
+  const isObjectEmpty = (objectName) => (
+    objectName
+      && Object.keys(objectName).length === 0
+      && objectName.constructor === Object
+  );
+
+  if (isObjectEmpty(user)) {
+    return <Loading />;
+  }
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+  return children;
+}
+
+export default ProtectedRoute;
