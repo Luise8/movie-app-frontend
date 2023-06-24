2e483539713962d28671ff396ff83aab4e856e2d
luis <luistenan@gmail.com>
2023-06-24 17:11:13 -0400

    Update the tests to pass after the changes made

--- src/components/list-movie-grid/index.test.jsx
+++ src/components/list-movie-grid/index.test.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { MemoryRouter } from 'react-router-dom';
-import moviesPopularExample from 'src/utils/movies-popular.example';
-import moviesRatedExample from 'src/utils/movies-rated-example';
-import linkRoutes from 'src/utils/link-routes';
-import appResourcesPath from 'src/utils/app-resources-path';
 import ListGridMovies from 'src/components/list-movie-grid';
 import CardMovieSmall from 'src/components/card-movie-small';
+import testDbHelpers from 'src/utils/test-db-helpers';
 
 jest.mock('src/components/card-movie-small/index.jsx', () => ({
   __esModule: true,
@@ -19,22 +15,22 @@ beforeEach(() => {
 });
 it('right render of data and right calls of child item component', () => {
   render(
-    <ListGridMovies list={moviesPopularExample.results} />,
+    <ListGridMovies list={testDbHelpers.moviesPopular.results} />,
   );
-  const indexLastElement = moviesPopularExample.results.length - 1;
-  expect(CardMovieSmall).toHaveBeenCalledTimes(moviesPopularExample.results.length);
+  const indexLastElement = testDbHelpers.moviesPopular.results.length - 1;
+  expect(CardMovieSmall).toHaveBeenCalledTimes(testDbHelpers.moviesPopular.results.length);
   expect(CardMovieSmall.mock.calls[0][0])
-    .toEqual({ cardData: { ...moviesPopularExample.results[0] } });
+    .toEqual({ cardData: { ...testDbHelpers.moviesPopular.results[0] } });
 
   expect(CardMovieSmall.mock.calls[indexLastElement][0])
-    .toEqual({ cardData: { ...moviesPopularExample.results[indexLastElement] } });
+    .toEqual({ cardData: { ...testDbHelpers.moviesPopular.results[indexLastElement] } });
 
-  expect(screen.getAllByTestId('card-movie-small-link')).toHaveLength(moviesPopularExample.results.length);
+  expect(screen.getAllByTestId('card-movie-small-link')).toHaveLength(testDbHelpers.moviesPopular.results.length);
 });
 
 it('right classes', () => {
   render(
-    <ListGridMovies list={[moviesPopularExample.results[0]]} />,
+    <ListGridMovies list={[testDbHelpers.moviesPopular.results[0]]} />,
   );
   const items = screen.getAllByTestId('card-grid-items-small');
   expect(items).toHaveLength(1);
