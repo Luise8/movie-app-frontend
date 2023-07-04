ee883a606631c1d3665f132095def23f95258b11
luis <luistenan@gmail.com>
2023-07-04 02:44:41 -0400

    Add error page
    Add component, tests and styles.

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,7 +1,48 @@
+import { Button, Container } from '@mui/material';
 import React from 'react';
+import appResourcesPath from 'src/utils/app-resources-path';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import 'src/pages/error/styles.css';
+import { Link } from 'react-router-dom';
+import { useUserAuth } from 'src/context/auth';
+import Loading from 'src/components/loading';
+import PageLayout from 'src/components/page-layout';
+import helperFunctions from 'src/utils/helper-functions';
 
 export default function Error() {
+  /* const { user } = useUserAuth(); */
+  const user = null;
+
+  if (helperFunctions.isObjectEmpty(user)) return <Loading />;
+
   return (
-    <div>Error</div>
+    <PageLayout>
+      <Container className="error" data-testid="page">
+        <Grid container className="error-container" data-testid="error-container">
+          <Grid item className="error-container-image" data-testid="error-container-image">
+            <img aria-hidden="true" src={appResourcesPath.errorIcon} alt="Sign with the word oops" />
+          </Grid>
+          <Grid item>
+            <Typography variant="h2" component="h1" color="error.dark" textAlign="center">
+              Something wrong
+            </Typography>
+          </Grid>
+          <Grid>
+            <Button
+              component={Link}
+              variant="contained"
+              to="/"
+              color="error"
+              sx={{
+                color: 'background.default',
+              }}
+            >
+              Go back home
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </PageLayout>
   );
 }
---
+++ src/pages/error/index.test.jsx
@@ -0,0 +1,95 @@
+import {
+  act,
+  render, screen, waitFor, within,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import Error from 'src/pages/error';
+import { MemoryRouter } from 'react-router-dom';
+import darkTheme from 'src/utils/darkTheme';
+import { ThemeProvider } from '@mui/material';
+
+let mockCurrentUser = {
+  id: '123',
+  username: 'userNumber1',
+};
+// mock useUserAuth
+jest.mock('src/context/auth', () => ({
+  useUserAuth: jest.fn(() => ({
+    user: mockCurrentUser,
+  })),
+}));
+
+jest.mock('src/components/page-layout', () => ({
+  __esModule: true,
+  default: jest.fn(({ children }) => <div data-testid="page-layout">{children}</div>),
+}));
+
+it('right render of data', async () => {
+  render(
+    <MemoryRouter>
+      <Error />
+    </MemoryRouter>,
+  );
+
+  const pageLayout = screen.getByTestId('page-layout');
+  expect(pageLayout).toBeInTheDocument();
+
+  const page = within(pageLayout).getByTestId('page');
+  expect(page).toBeInTheDocument();
+
+  const img = screen.getByAltText(/sign with the word oops/i);
+  expect(img).toHaveAttribute('aria-hidden', 'true');
+
+  expect(screen.getByRole('heading', {
+    name: /Something wrong/i,
+    level: 1,
+  })).toBeInTheDocument();
+
+  expect(screen.getByRole('link', {
+    name: /Go back home/i,
+  })).toBeInTheDocument();
+});
+
+it('right render of loading state', () => {
+  // Change to initial user state
+  mockCurrentUser = {};
+
+  render(
+    <MemoryRouter>
+      <Error />
+    </MemoryRouter>,
+  );
+  expect(screen.getByText(/loading.../i)).toBeInTheDocument();
+  expect(screen.queryByTestId('page-layout')).not.toBeInTheDocument();
+});
+
+it('right classes and inline styles', () => {
+  // logged user
+  mockCurrentUser = {
+    id: '123',
+    username: 'userNumber1',
+  };
+  render(
+    <MemoryRouter>
+      <ThemeProvider theme={darkTheme}>
+        <Error />
+      </ThemeProvider>
+    </MemoryRouter>,
+  );
+
+  expect(screen.getByTestId('page')).toHaveClass('error');
+
+  expect(screen.getByTestId('error-container')).toHaveClass('error-container');
+
+  expect(screen.getByTestId('error-container-image')).toHaveClass('error-container-image');
+
+  expect(screen.getByRole('heading', {
+    name: /Something wrong/i,
+    level: 1,
+  })).toHaveStyle(`color: ${darkTheme.palette.error.dark}`);
+
+  expect(screen.getByRole('link', {
+    name: /Go back home/i,
+  })).toHaveStyle(`color: ${darkTheme.palette.background.default}`);
+});
