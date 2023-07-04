984fd3b65b79c1a824734fdb357d383378fd37e0
luis <luistenan@gmail.com>
2023-07-03 22:21:56 -0400

    Fix bug of wrong path to resources with absolute paths

--- src/utils/app-resources-path.js
+++ src/utils/app-resources-path.js
@@ -1,8 +1,14 @@
+import websiteIcon from 'src/assets/my-website-logo.png';
+import userDefaultIcon from 'src/assets/user-default-icon.svg';
+import errorIcon from 'src/assets/error.png';
+import noImageAvailable from 'src/assets/no-image-available-card-movie-small.jpg';
+
 const appResourcesPath = {
   cardMovieSmall: {
-    noImageAvailable: 'src/assets/no-image-available-card-movie-small.jpg',
+    noImageAvailable,
   },
-  userDefaultIcon: 'src/assets/user-default-icon.svg',
-  myWebsiteIcon: 'src/assets/my-website-logo.png',
+  userDefaultIcon,
+  myWebsiteIcon: websiteIcon,
+  errorIcon,
 };
 export default appResourcesPath;
