d6861d4bd3c48b25b8c02f1d8035de5e735f95aa
luis <luistenan@gmail.com>
2023-07-04 23:10:46 -0400

    Add UserRates page
    Add component, styles and tests.
    Add hook to get data by this component.

--- src/pages/user-rates/index.jsx
+++ src/pages/user-rates/index.jsx
@@ -1,7 +1,92 @@
-import React from 'react';
+import { LoadingButton } from '@mui/lab';
+import {
+  Box, Button, Container, Grid, Typography,
+} from '@mui/material';
+import React, { useMemo } from 'react';
+import AddCircleOutlineSharpIcon from '@mui/icons-material/AddCircleOutlineSharp';
+import useRatesUser from 'src/hooks/use-rates-user';
+import { Link, Navigate, useParams } from 'react-router-dom';
+import throttle from 'just-throttle';
+import { useUserAuth } from 'src/context/auth';
+import Loading from 'src/components/loading';
+import PageLayout from 'src/components/page-layout';
+import CardRateUser from 'src/components/card-rate-user';
+import helperFunctions from 'src/utils/helper-functions';
 
 export default function UserRates() {
+  const { user } = useUserAuth();
+  const { id } = useParams();
+  const {
+    data, loading, loadingNextPage, setPage, error,
+  } = useRatesUser({ id });
+
+  const debounceHandleNextPage = useMemo(() => throttle(
+    () => setPage((prevPage) => prevPage + 1),
+    3000,
+    { leading: true },
+  ), [setPage]);
+
+  if (loading || helperFunctions.isObjectEmpty(user)) return <Loading />;
+
+  if (error?.status === 404) return <Navigate to="/404" />;
+
+  if (error) return <Navigate to="/error" />;
+
   return (
-    <div>UserRates</div>
+    <PageLayout>
+      <Container className="page" data-testid="page-user-rates">
+        <Box className="page-container-button-user" data-testid="page-container-button-user">
+          <Button
+            component={Link}
+            to={`/user/${id}`}
+            sx={{
+              color: 'primary.light',
+            }}
+          >
+            See profile of
+            {' '}
+            { data.user_details.username }
+          </Button>
+        </Box>
+        <Typography sx={{ typography: { sm: 'h2', xs: 'h3' } }} component="h1" color="secondary.light" align="center">
+          Rates
+        </Typography>
+        <Typography variant="h5" component="h2">{`Results: ${data.results?.length}`}</Typography>
+        <Grid
+          container
+          sx={{
+            alignItems: 'center',
+          }}
+          rowSpacing={2}
+          direction="column"
+        >
+          {data.results.map((item) => (
+            <Grid
+              item
+              xs={12}
+              key={item.id}
+            >
+              <CardRateUser data={item} />
+            </Grid>
+          ))}
+        </Grid>
+        <Box
+          className="page-container-button-more"
+          data-testid="page-container-button-more"
+        >
+          <LoadingButton
+            color="secondary"
+            onClick={debounceHandleNextPage}
+            loading={loadingNextPage}
+            loadingPosition="end"
+            endIcon={<AddCircleOutlineSharpIcon />}
+            variant={loadingNextPage || data.next_page === '' ? 'outlined' : 'contained'}
+            disabled={data.next_page === ''}
+          >
+            More
+          </LoadingButton>
+        </Box>
+      </Container>
+    </PageLayout>
   );
 }
---
+++ src/pages/user-rates/index.test.jsx
@@ -0,0 +1,255 @@
+import {
+  act, render, screen, waitFor, within,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import { getRatesUser } from 'src/services/get-data';
+import userEvent from '@testing-library/user-event';
+import testDbHelpers from 'src/utils/test-db-helpers';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import darkTheme from 'src/utils/darkTheme';
+import Error from 'src/pages/error';
+import linkRoutes from 'src/utils/link-routes';
+import UserRates from 'src/pages/user-rates';
+import CardRateUser from 'src/components/card-rate-user';
+
+const mockCurrentUser = {
+  id: '123',
+  username: 'userNumber1',
+};
+// mock useUserAuth
+jest.mock('src/context/auth', () => ({
+  useUserAuth: jest.fn(() => ({
+    user: mockCurrentUser,
+  })),
+}));
+
+const mockData = testDbHelpers.ratesUser;
+// mock get data service
+jest.mock('src/services/get-data', () => ({
+  getRatesUser: jest.fn(() => (Promise.resolve(mockData))),
+}));
+
+// Mock Item
+jest.mock('src/components/card-rate-user', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div data-testid="card-rate-user" />),
+}));
+
+// Mock PageLayout
+jest.mock('src/components/page-layout', () => ({
+  __esModule: true,
+  default: jest.fn(({ children }) => <div data-testid="page-layout">{children}</div>),
+}));
+
+// Clear mocks
+beforeEach(async () => {
+  getRatesUser.mockClear();
+  CardRateUser.mockClear();
+});
+
+it('render right of initial page data after loading state', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter initialEntries={[`/user/${mockCurrentUser.id}/rates`]}>
+        <ThemeProvider theme={darkTheme}>
+          <Routes>
+            <Route path="/user/:id/rates" element={<UserRates />} />
+          </Routes>
+        </ThemeProvider>
+        ,
+      </MemoryRouter>,
+    );
+  });
+  expect(getRatesUser).toBeCalled();
+
+  expect(screen.queryByLabelText(/Loading.../i)).not.toBeInTheDocument();
+
+  const pageLayout = screen.getByTestId('page-layout');
+  expect(pageLayout).toBeInTheDocument();
+
+  const page = within(pageLayout).getByTestId('page-user-rates');
+  expect(page).toBeInTheDocument();
+
+  const LinkUserMatcher = `see profile of ${mockData.user_details.username}`;
+
+  const linkUser = screen.getByRole('link', {
+    name: new RegExp(LinkUserMatcher, 'i'),
+  });
+  expect(linkUser).toBeInTheDocument();
+
+  expect(linkUser).toHaveAttribute('href', linkRoutes.userProfile(mockCurrentUser.id));
+
+  expect(screen.getByRole('heading', {
+    name: /rates/i,
+    level: 1,
+  })).toBeInTheDocument();
+
+  expect(screen.getByText(`Results: ${mockData.results.length}`)).toBeInTheDocument();
+
+  const cardsRateUser = screen.getAllByTestId('card-rate-user');
+  expect(cardsRateUser).toHaveLength(mockData.results.length);
+
+  expect(CardRateUser).toHaveBeenCalledWith({
+    data: { ...mockData.results[0] },
+  }, {});
+
+  expect(CardRateUser).toHaveBeenLastCalledWith({
+    data: { ...mockData.results[1] },
+  }, {});
+
+  expect(screen.getByRole('button', {
+    name: /more/i,
+  })).toBeInTheDocument();
+});
+
+it('Render right initial loading state', async () => {
+  render(
+    <MemoryRouter>
+      <ThemeProvider theme={darkTheme}>
+        <UserRates />
+      </ThemeProvider>
+      ,
+    </MemoryRouter>,
+  );
+  await waitFor(() => {
+    expect(screen.getByLabelText(/Loading.../i)).toBeInTheDocument();
+  });
+});
+
+it('Render the correct error page when there is any error other than 404', async () => {
+  getRatesUser.mockImplementationOnce(() => Promise.reject(new Error('something wrong')));
+
+  await act(async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider theme={darkTheme}>
+          <Routes>
+            <Route path="/" element={<UserRates />} />
+            <Route path="/error" element={<Error />} />
+          </Routes>
+        </ThemeProvider>
+        ,
+      </MemoryRouter>,
+    );
+  });
+  expect(getRatesUser).toBeCalled();
+
+  expect(screen.queryByLabelText(/Loading.../i)).not.toBeInTheDocument();
+
+  expect(screen.queryByTestId(/page-user-rates/i)).not.toBeInTheDocument();
+
+  expect(screen.getByTestId(/^error-container$/i)).toBeInTheDocument();
+});
+
+it('right handle of pagintation', async () => {
+  // Change mock get data service to give max only 1 item per
+  // page to make more easy pagination tests. Real implementation gives max 20.
+
+  // Fist page
+  const pageOne = {
+    ...testDbHelpers.ratesUser, page: 0, page_zise: 1, next_page: 'next page url', results: [testDbHelpers.ratesUser.results[0]],
+  };
+
+  // Second Page
+  const pageTwo = {
+    ...testDbHelpers.ratesUser, page: 1, page_zise: 1, prev_page: 'perv page url', next_page: '', results: [testDbHelpers.ratesUser.results[1]],
+  };
+  getRatesUser
+    .mockImplementationOnce(() => Promise.resolve(pageOne))
+    .mockImplementationOnce(() => Promise.resolve(pageTwo));
+
+  await act(async () => {
+    render(
+      <MemoryRouter initialEntries={[`/user/${mockCurrentUser.id}/rates`]}>
+        <ThemeProvider theme={darkTheme}>
+          <Routes>
+            <Route path="/user/:id/rates" element={<UserRates />} />
+          </Routes>
+        </ThemeProvider>
+        ,
+      </MemoryRouter>,
+    );
+  });
+
+  const pageLayout = screen.getByTestId('page-layout');
+  expect(pageLayout).toBeInTheDocument();
+
+  const page = within(pageLayout).getByTestId('page-user-rates');
+  expect(page).toBeInTheDocument();
+
+  // Only 1 result
+  expect(screen.getByText(`Results: ${pageOne.results.length}`)).toBeInTheDocument();
+
+  // The button more is not disabled when there are more data
+  expect(screen.getByRole('button', {
+    name: /more/i,
+  })).not.toHaveAttribute('disabled');
+
+  expect(CardRateUser).toHaveBeenCalledWith({
+    data: { ...pageOne.results[0] },
+  }, {});
+
+  const user = userEvent.setup();
+  const buttonMore = screen.getByRole('button', {
+    name: /more/i,
+  });
+
+  await user.click(buttonMore);
+
+  await waitFor(() => {
+    expect(CardRateUser).toHaveBeenLastCalledWith({
+      data: { ...pageTwo.results[0] },
+    }, {});
+  });
+
+  // At the end the result is incremented
+  expect(screen.getByText(`Results: ${pageOne.results.length + pageTwo.results.length}`)).toBeInTheDocument();
+
+  const cardsRateUser = screen.getAllByTestId('card-rate-user');
+  expect(cardsRateUser).toHaveLength(pageOne.results.length + pageTwo.results.length);
+
+  // The button more is disabled when there are not more data
+  expect(screen.getByRole('button', {
+    name: /more/i,
+  })).toHaveAttribute('disabled');
+});
+
+it('right classes and inline styles', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter initialEntries={[`/user/${mockCurrentUser.id}/rates`]}>
+        <ThemeProvider theme={darkTheme}>
+          <Routes>
+            <Route path="/user/:id/rates" element={<UserRates />} />
+          </Routes>
+        </ThemeProvider>
+        ,
+      </MemoryRouter>,
+    );
+  });
+
+  expect(screen.getByTestId('page-user-rates')).toHaveClass('page');
+
+  expect(screen.getByTestId('page-container-button-user')).toHaveClass('page-container-button-user');
+
+  expect(screen.getByTestId('page-container-button-more')).toHaveClass('page-container-button-more');
+
+  const LinkUserMatcher = `see profile of ${mockData.user_details.username}`;
+  const linkUser = screen.getByRole('link', {
+    name: new RegExp(LinkUserMatcher, 'i'),
+  });
+
+  expect(linkUser).toHaveStyle(`color: ${darkTheme.palette.primary.light}`);
+
+  expect(screen.getByRole('heading', {
+    name: /rates/i,
+    level: 1,
+  })).toHaveStyle(`color: ${darkTheme.palette.secondary.light}`);
+
+  // atribute of Button color secondary and variant outlined
+  expect(screen.getByRole('button', {
+    name: /more/i,
+  })).toHaveClass('MuiButton-outlinedSecondary');
+});
