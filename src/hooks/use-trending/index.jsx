7a07526648135ab0b67e08aef6ff92cc039bd744
luis <luistenan@gmail.com>
2023-07-06 12:00:09 -0400

    Add Trending page
    Add component, styles and tests.
    Add hook to get data by this component.

---
+++ src/hooks/use-trending/index.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import {
+  getTrendingMovies,
+} from 'src/services/get-data';
+import helperFunctions from 'src/utils/helper-functions';
+
+export default function useTrending() {
+  const INITIALPAGE = 1;
+  const [data, setData] = useState({});
+  const [page, setPage] = useState(INITIALPAGE);
+  const [loading, setLoading] = useState(true);
+  const [loadingNextPage, setLoadingNextPage] = useState(false);
+  const [error, setError] = useState(undefined);
+
+  useEffect(() => {
+    let mounted = true;
+    async function fetchData() {
+      try {
+        if (mounted) {
+          setLoading(true);
+        }
+        const dataFetched = await getTrendingMovies(INITIALPAGE);
+        if (mounted) {
+          setData(dataFetched);
+          setError(false);
+          setLoading(false);
+        }
+      } catch (e) {
+        if (mounted) {
+          setError(e);
+          setLoading(false);
+        }
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    let mounted = true;
+
+    async function fetchData() {
+      try {
+        if (page === INITIALPAGE) {
+          return;
+        }
+
+        if (mounted) {
+          setLoadingNextPage(true);
+        }
+
+        const dataFetched = await getTrendingMovies(page);
+        if (mounted) {
+          setData((prevData) => {
+            const initialMovies = prevData.results.slice();
+            const resultsMaybeDuplicated = initialMovies.concat(dataFetched.results);
+            return { ...dataFetched, results: helperFunctions.getUniqueItemsByProperties(resultsMaybeDuplicated, 'id') };
+          });
+          setError(false);
+          setLoadingNextPage(false);
+        }
+      } catch (e) {
+        if (mounted) {
+          setError(e);
+          setLoadingNextPage(false);
+        }
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      mounted = false;
+    };
+  }, [page]);
+
+  return {
+    data, loading, loadingNextPage, setPage, error,
+  };
+}
