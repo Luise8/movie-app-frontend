268680d9856a4501e9a22758db7f0eb02dfb20fb
luis <luistenan@gmail.com>
2023-07-06 11:57:25 -0400

    Add Popular page
    Add component, styles and tests.
    Add hook to get data by this component.

--- src/pages/popular/index.jsx
+++ src/pages/popular/index.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import { LoadingButton } from '@mui/lab';
+import {
+  Box, Container, Typography,
+} from '@mui/material';
+import React, { useMemo } from 'react';
+import AddCircleOutlineSharpIcon from '@mui/icons-material/AddCircleOutlineSharp';
+import { Navigate } from 'react-router-dom';
+import throttle from 'just-throttle';
+import { useUserAuth } from 'src/context/auth';
+import Loading from 'src/components/loading';
+import PageLayout from 'src/components/page-layout';
+import helperFunctions from 'src/utils/helper-functions';
+import ListGridMovies from 'src/components/list-movie-grid';
+import usePopular from 'src/hooks/use-popular';
 
 export default function Popular() {
+  const { user } = useUserAuth();
+  const {
+    data, loading, loadingNextPage, setPage, error,
+  } = usePopular();
+
+  const debounceHandleNextPage = useMemo(() => throttle(
+    () => setPage((prevPage) => prevPage + 1),
+    3000,
+    { leading: true },
+  ), [setPage]);
+
+  if (loading || helperFunctions.isObjectEmpty(user)) return <Loading />;
+
+  if (error?.status === 404) return <Navigate to="/404" />;
+
+  if (error) return <Navigate to="/error" />;
+
   return (
-    <div>Popular</div>
+    <PageLayout>
+      <Container className="page" data-testid="page-popular">
+        <Typography sx={{ typography: { sm: 'h2', xs: 'h3' } }} component="h1" color="secondary.light" textAlign="center">
+          Popular Movies
+        </Typography>
+        <Typography variant="h5" component="h2">{`Total: ${data?.total_results}`}</Typography>
+        <ListGridMovies list={data.results} />
+        <Box className="page-container-button-more" data-testid="page-container-button-more">
+          <LoadingButton
+            color="secondary"
+            onClick={debounceHandleNextPage}
+            loading={loadingNextPage}
+            loadingPosition="end"
+            endIcon={<AddCircleOutlineSharpIcon />}
+            variant={loadingNextPage || data.next_page === '' ? 'outlined' : 'contained'}
+            disabled={data.next_page === ''}
+          >
+            More
+          </LoadingButton>
+        </Box>
+      </Container>
+    </PageLayout>
   );
 }
